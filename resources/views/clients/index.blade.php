aeb7f5987e91776ef31941f9eea2d580aacaadaf
Oscar Navarrete <oscarit@yascemail.com>
2020-03-06 15:32:06 -0600

    change error

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -23,11 +23,9 @@
                                     <input type="text" class="form-control" id="search">
                                     <button type="button" class="btn btn-primary search-query"><i class="fa fa-search"></i></button>
                                 </div>
-                                
+                                @include('clients.table')
                             </div>
-                            @include('clients.table')
                         </div>
-                        
                     </div>
                 </div>
 </div>
