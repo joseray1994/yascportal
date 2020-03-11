d68c6b8335f05eb445297714fc492063f4fc233a
Maleny Montalvo <malenyit@yascemail.com>
2020-03-11 08:39:37 -0600

    Avances de training, inserta en duration, schedule, detalleschedule tables

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -11,6 +11,9 @@ use App\User;
 use App\User_info;
 use App\TypeUserModel;
 use App\ClientModel;
+use App\TorCDuration;
+use App\ScheduleTrainingModel;
+use App\TrainingDetailModel;
 use Illuminate\Support\Facades\Hash;
 
 
@@ -165,83 +168,195 @@ class TrainingController extends Controller
         }
     }
 
+    public function generateEnd_training(Request $request)
+    { 
+        $num=0;
+
+        if ($request->start_training!=null && $request->end_training!=null) {
+            $end_training=$request->end_training;
+            $end_training2=$request->end_training;
+            $start=$request->start_training;
+            $start2=$request->start_training;
+
+            $start3=Carbon::parse($start2);
+            $end=Carbon::parse($end_training2);
+
+            $weeks= $end->weekOfYear - $start3->weekOfYear;
+            $weeks+=1;
+            $num=['num'=>$weeks, 'flag'=>1];
+
+        }elseif ($request->start_training!=null && $request->numWeek > 0) {
+            $date=Carbon::parse($request->start_training);
+            $date2=Carbon::parse($request->start_training);
+            $start=$date->startOfWeek(Carbon::SUNDAY);
+            $start2=$date->startOfWeek(Carbon::SUNDAY);
+            $end=$date2->endOfWeek(Carbon::SATURDAY);
+            $fin=$start2->addWeek($request->numWeek);
+            $end_training=$fin->subDay(1)->format('Y-m-d');
+            $num=['num'=>$request->numWeek, 'flag'=>2];
+        }
+        return response()->json(['start_training'=>$request->start_training, 'end_training'=>$end_training,'num'=>$num]);
+    }
+
     public function store(Request $request)
     {     
-        $setting_id="";
-        dd($request);
+        //in TorCDuration
+        //type=1 está en training , type=2 está en coaching
+    //--------------------------//
+    //in ScheduleModel
+        //status=1 horario habilitado, status=2 el horario todavia no se usa porque está en coaching
+    //--------------------------//
         
         $correo=$request->email.'@yasemail.com';
-        $user =  User::Create([
-            'id_type_user'=>11,
-            'id_status'=>1,
-            'nickname'=>"",
-            'email'=>$correo,
-            'password'=>Hash::make('123'),
-        ]);
+        // $user =  User::Create([
+        //     'id_type_user'=>11,
+        //     'id_status'=>1,
+        //     'nickname'=>"",
+        //     'email'=>$correo,
+        //     'password'=>Hash::make('123'),
+        // ]);
 
-        $User_info =  User_info::Create([
-            'id_user'=>$user->id,
-            'name'=>$request->name,
-            'last_name'=>$request->last_name,
-            'address'=>'',
-            'phone'=>$request->phone,
-            'emergency_contact_name'=>$request->emergency_contact_name,
-            'emergency_contact_phone'=>$request->emergency_contact_phone,
-            'notes'=>$request->notes,
-            'description'=>$request->description,
-            'gender'=>$request->gender,
-            'birthdate'=>$request->birthdate,
-            'profile_picture'=>"",
-            'biotime_status'=>"",
-            'access_code'=>"",
-            'entrance_date'=>"2020-01-01",
-        ]);
+        // $User_info =  User_info::Create([
+        //     'id_user'=>$user->id,
+        //     'name'=>$request->name,
+        //     'last_name'=>$request->last_name,
+        //     'address'=>'',
+        //     'phone'=>$request->phone,
+        //     'emergency_contact_name'=>$request->emergency_contact_name,
+        //     'emergency_contact_phone'=>$request->emergency_contact_phone,
+        //     'notes'=>$request->notes,
+        //     'description'=>$request->description,
+        //     'gender'=>$request->gender,
+        //     'birthdate'=>$request->birthdate,
+        //     'profile_picture'=>"",
+        //     'biotime_status'=>"",
+        //     'access_code'=>"",
+        //     'entrance_date'=>"2020-01-01",
+        // ]);
+            $schedulearray=[];
+        if ($request->end_coaching!= null) {
+            $start_coaching=Carbon::parse($request->end_training)->addDay(1);
 
-        $schedule =  ScheduleModel::Create([
-            'id_operator'=>$user->id,
-            // 'id_client'=> ,
-            // 'mat'=> ,
-            // 'date_start'=> ,
-            // 'date_end'=> ,
-            // 'type_daily'=> ,
-            // 'week'=> ,
-            // 'mount'=> ,
-            // 'year'
-            // 'status'=> ,
-    
-        ]);
+            $id_training =  TorCDuration::Create([
+                'mat'=>'TCD',
+                'id_trainer'=>$request->id_trainer,
+                'date_start'=> $request->start_training,
+                'date_end'=> $request->end_training,
+                'type'=>1 ,
+                'status'=>1 ,
+            ]);
+            array_push($schedulearray, array('id'=>$id_training->id,'status'=>1));
 
-        $training_detail =  TrainingDetailModel::Create([
-            // 'mat'=> ,
-            'id_operator'=> $user->id,
-            'id_schedule'=>$schedule->id,
-            // 'id_day'=> ,
-            // 'start_time'=> ,
-            // 'end_time'=> ,
-            // 'options'=> ,
-            // 'status'=> ,
-        ]);
+            $id_coaching =  TorCDuration::Create([
+                'mat'=>'TCD',
+                'id_trainer'=>$request->id_trainer,
+                'date_start'=> $start_coaching,
+                'date_end'=> $request->end_coaching,
+                'type'=>2 ,
+                'status'=>1 ,
+            ]);
+            array_push($schedulearray, array('id'=>$id_coaching->id,'status'=>2));
 
-        $daysOFF =  DayOffModel::Create([
-            'id_schedule'=>$schedule->id,
-            // 'id_day'=>,
-        ]);
+        }else{
+            $id_training =  TorCDuration::Create([
+                'mat'=>'TCD',
+                'id_trainer'=>$request->id_trainer,
+                'date_start'=> $request->start_training,
+                'date_end'=> $request->end_training,
+                'type'=>1 ,
+                'status'=>1 ,
+            ]);
+            array_push($schedulearray, array('id'=>$id_training->id,'status'=>1));
+        }
+        $length_array=count($schedulearray);
 
-        $mirror_userScheduleDetail =  MirrorUserScheduleDetailModel::Create([
-            'id_schedule'=>$schedule->id,
-            'id_operator'=> $user->id,
-            // 'id_day'=> ,
-            // 'mat'=> ,
-            // 'time_start'=> ,
-            // 'time_end'=> ,
-            // 'type_daily'=> ,
-            // 'option'=> ,
-            // 'status'=> ,
-        ]);
+        for ($i=0; $i < $length_array; $i++) { 
+                $start_week='';
+                $start_training=Carbon::parse($request->start_training);
+                $day_start=$start_training->dayOfWeek;
+                $day_end=Carbon::parse($request->end_training)->dayOfWeek;
+                $start_week=Carbon::parse($request->start_training)->startOfWeek(Carbon::SUNDAY);
+                $end_week=Carbon::parse($request->start_training)->endOfWeek(Carbon::SATURDAY);
 
-        $result = TrainingController::getResult($user->id);
+            for ($j=0; $j < $request->numWeek; $j++) { 
+            
 
-        return response()->json($result);
+                $weekYears=$end_week->weekOfYear;
+
+                $month_I=Carbon::parse($start_week)->month;
+                $month_F=Carbon::parse($end_week)->month;
+
+                $year=Carbon::parse($start_week)->year;
+                $type='';
+                if ($schedulearray[$i]['status']==1) {
+                    $type=2;
+                }else{
+                    $type=3;
+                }
+                $schedule =  ScheduleTrainingModel::Create([
+                    'id_operator'=>3,
+                    'id_torcduration'=> $schedulearray[$i]['id'] ,
+                    // 'id_operator'=>$user->id,
+                    'id_client'=>$request->id_client ,
+                    'mat'=>'SCH',
+                    'date_start'=>$start_week ,
+                    'date_end'=> $end_week,
+                    'type_schedule'=>$type ,
+                    'week'=> $weekYears,
+                    'month'=>$month_I ,
+                    'year'=>$year,
+                    'status'=> 1 ,
+            
+                ]);
+                for ($k=0; $k <7 ; $k++) { 
+                    $training_detail =  TrainingDetailModel::Create([
+                    'mat'=>'TSD' ,
+                    'id_user'=> 3,
+                    'id_schedule'=>$schedule->id,
+                    'id_day'=>$k ,
+                    'start_time'=>$request->start ,
+                    'end_time'=>$request->end ,
+                    'options'=>0 ,
+                    'status'=>1 ,
+                        ]);
+                }
+               
+                $start_week->addWeek();
+                $end_week->addWeek();
+            }
+                $Schedule_id_start=ScheduleTrainingModel::where('date_start','<=',$request->start_training)
+                ->where('date_end','>=',$request->start_training)
+                ->first();
+                $NDay_start=TrainingDetailModel::where('id_schedule',$Schedule_id_start->id)->where('id_day','<',$day_start)->delete();
+
+                $Schedule_id_end=ScheduleTrainingModel::where('date_end','>=',$request->end_training)->where('id_torcduration', $schedulearray[$i]['id'] )
+                ->first();
+                $NDay_end=TrainingDetailModel::where('id_schedule',$Schedule_id_end->id)->where('id_day','>',$day_end)->delete();
+
+            
+            
+        }
+
+        // $daysOFF =  DayOffModel::Create([
+        //     'id_schedule'=>$schedule->id,
+        //     // 'id_day'=>,
+        // ]);
+
+        // $mirror_userScheduleDetail =  MirrorUserScheduleDetailModel::Create([
+        //     'id_schedule'=>$schedule->id,
+        //     'id_operator'=> $user->id,
+        //     // 'id_day'=> ,
+        //     // 'mat'=> ,
+        //     // 'time_start'=> ,
+        //     // 'time_end'=> ,
+        //     // 'type_daily'=> ,
+        //     // 'option'=> ,
+        //     // 'status'=> ,
+        // ]);
+
+        // $result = TrainingController::getResult($user->id);
+
+        // return response()->json($result);
 
       
            
@@ -265,22 +380,22 @@ class TrainingController extends Controller
 
     public function update(Request $request, $settings_id)
     {
-        $settingValidation = SettingsController::validateSettingsExists($request->name,$request->id_option);
-        if(!$settingValidation){
-            SettingsController::validateSettings($request,$settings_id);
-            $setting = SettingsModel::find($settings_id);
-            $setting->name = $request->name;
-            $setting->id_option = $request->id_option;
-            $setting->status=1;
-            $setting->save();
-            $dataSettings= SettingsController::data_settings($setting->id);
+        // // $settingValidation = SettingsController::validateSettingsExists($request->name,$request->id_option);
+        // if(!$settingValidation){
+        //     // SettingsController::validateSettings($request,$settings_id);
+        //     $setting = SettingsModel::find($settings_id);
+        //     $setting->name = $request->name;
+        //     $setting->id_option = $request->id_option;
+        //     $setting->status=1;
+        //     $setting->save();
+        //     $dataSettings= SettingsController::data_settings($setting->id);
 
-            return response()->json($dataSettings);
-        }else{
-            $msg='Another option already has that name and that type';
-            $data=['No'=>1,'msg'=>$msg];
-            return response()->json($data);
-        }
+        //     return response()->json($dataSettings);
+        // }else{
+        //     $msg='Another option already has that name and that type';
+        //     $data=['No'=>1,'msg'=>$msg];
+        //     return response()->json($data);
+        // }
     }
 
     public function destroy($settings_id)
